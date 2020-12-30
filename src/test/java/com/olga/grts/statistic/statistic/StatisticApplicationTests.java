3f761d83c932488ae6614676f700e0cf6d0c9030
trowqe <trowqe@gmail.com>
2020-12-30 14:12:39 +0300

    init spring

---
+++ src/test/java/com/olga/grts/statistic/statistic/StatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olga.grts.statistic.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
